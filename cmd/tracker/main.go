856ed93bff0b18c6f4d409d6ad516e92020586ce
Manav Mahesh Sanger <karnophile@gmail.com>
2026-01-07 16:04:17 +0530

    feat: implement list command and refactor main with tabwriter

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -5,21 +5,15 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"text/tabwriter"
 
 	"github.com/karnop/expense-tracker/internal/store"
 )
 
 func main() {
-	// Initialize the FileStore
+	// Initializing the FileStore
 	// We will store data in a file named "expenses.json" in the current directory
 	storage := store.NewFileStore("expenses.json")
-
-	// Defining the add subcommand
-	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
-
-	// defining flags for add subcommand
-	addCategory := addCmd.String("category", "", "The category of the expense.")
-	addAmount := addCmd.Float64("amount", 0, "The amount of the expense")
 
 	// checking if the user provides enough arguments
 	if len(os.Args) < 2 {
@@ -29,7 +23,27 @@ func main() {
 
 	switch os.Args[1] {
 	case "add":
-		addCmd.Parse(os.Args[2:])
+		runAdd(storage)
+
+	case "list":
+		runList(storage)
+
+	default:
+		fmt.Println("expected 'add' subcommand")
+		os.Exit(1)
+	}
+}
+
+
+func runAdd(storage *store.FileStore) {
+	// Defining the add subcommand
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+
+	// defining flags for add subcommand
+	addCategory := addCmd.String("category", "", "The category of the expense.")
+	addAmount := addCmd.Float64("amount", 0, "The amount of the expense")
+
+	addCmd.Parse(os.Args[2:])
 
 		// validating input
 		if *addCategory == "" || *addAmount == 0 {
@@ -63,9 +77,25 @@ func main() {
 		}
 
 		fmt.Printf("Expense added successfully (ID: %d)\n", newExpense.ID)
+}
 
-	default:
-		fmt.Println("expected 'add' subcommand")
+func runList(storage *store.FileStore) {
+	expenses, err := storage.Load()
+	if err != nil {
+		fmt.Printf("Error loading expenses: %v\n", err)
 		os.Exit(1)
 	}
+
+	// Creating a new tabwriter
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+
+	// printing the header
+	fmt.Fprintln(w, "ID\tCategory\tAmount")
+
+	for _, e := range expenses {
+		fmt.Fprintf(w, "%d\t%s\t%.2f\n", e.ID, e.Category, e.Amount)
+	}
+
+	// flushing the writer to ensure everything is printed
+	w.Flush()
 }
